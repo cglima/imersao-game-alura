35016b0719c0b0d9810eaea225c9fd5fd2483d11
Cassiana <lima.cglima@gmail.com>
2020-06-23 20:11:19 -0300

    organizei a matriz no personagem.js, de forma genérica, substitui os números fixos por cálculo de matriz.

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,31 +1,60 @@
 class Personagem {
+
   constructor(imagem){
     this.imagem = imagem; 
-    this.matriz = [
+    /*this.matriz = [
         [0,0], [220,0], [440,0], [660,0],
         [0,270],[220,270], [440,270], [660,270],
         [0,540], [220,540], [440,540], [660,540],
         [0,810], [220,810], [440,810], [660,810],
-    ]
-    this.frameAtual = 0
+    ]*/
+
+    this.colunaSprite = 4;
+    this.linhaSprite = 4;
+    this.larguraImagem = 220;
+    this.alturaImagem = 270;
+    this.matrizSprite = Array();
+
+    this.frameAtual = 0;
+
+    let indice = 0;
+    let posicaoX;
+    let posicaoY;
+
+    for(let i = 0 ; i < this.colunaSprite ; i++) {
+        for(let j = 0 ; j < this.linhaSprite ; j++){
+          indice = this.colunaSprite * i + j;
+
+          posicaoX = this.larguraImagem * j;
+          posicaoY = this.alturaImagem * i;
+
+          this.matrizSprite[indice] = [posicaoX, posicaoY];
+      }
+    }
+
   }
-  
+
   exibe(){
-    image(this.imagem, 0, height - 135, 110, 135, 
-        this.matriz[this.frameAtual][0], 
-        this.matriz[this.frameAtual][1], 220, 270); 
-    
-    this.anima()
-    
+
+    let spriteX = this.matrizSprite[this.frameAtual][0];
+    let spriteY = this.matrizSprite[this.frameAtual][1];
+
+    //image(img, dx, dy, dWidth, dHeight, sx, sy, [sWidth], [sHeight]
+    image(
+      this.imagem,
+      0, height - 135,
+      110, 135,
+        spriteX, spriteY,
+        this.larguraImagem, this.alturaImagem
+    );
+
   }
-  
+
   anima(){
-    this.frameAtual++
+    this.frameAtual++;
     
-    if(this.frameAtual >= this.matriz.length - 1){
-      this.frameAtual = 0
-       }
-  
+    if(this.frameAtual >= this.matrizSprite.length - 1) {
+      this.frameAtual = 0;
+    }
   }
-  
 }
